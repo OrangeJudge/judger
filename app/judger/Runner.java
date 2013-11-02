c56861b1293a422763c53b28cfc9ccaa7ae0e5db
Zhou Xinzi <imdreamrunner@gmail.com>
2013-11-02 12:21:12 +0800

    change Runner

--- app/judger/Runner.java
+++ app/judger/Runner.java
@@ -1,6 +1,12 @@
 package judger;
 
+import judger.languages.LangC;
 import models.Submit;
+import org.h2.store.fs.FileUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Runner extends Thread {
     public Submit submit;
@@ -10,11 +16,7 @@ public class Runner extends Thread {
     }
 
     public void run() {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        compile();
         submit.status = 2;
         submit.save();
         System.out.println("ran " + submit.id);
@@ -23,6 +25,21 @@ public class Runner extends Thread {
     }
 
     public void compile() {
-
+        switch (submit.language) {
+            case 0: {
+                try {
+                    FileUtils.deleteRecursive("temp", true);
+                    (new File("temp/exroot")).mkdirs();
+                    PrintWriter writer = new PrintWriter(new File("temp/exroot/submit.c"));
+                    writer.print(submit.source);
+                    writer.close();
+                    LangC langC = new LangC();
+                    // langC.compile();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
     }
 }
