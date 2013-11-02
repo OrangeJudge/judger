2cb4ddae1aa9a8c47e64c7c890571bddd2c68608
Zhou Xinzi <imdreamrunner@gmail.com>
2013-11-02 16:17:20 +0800

    debugging Verify Error

--- app/judger/Runner.java
+++ app/judger/Runner.java
@@ -3,9 +3,11 @@ package judger;
 import judger.languages.LangC;
 import models.Submit;
 import org.h2.store.fs.FileUtils;
-import utils.OJException;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Date;
 
 public class Runner implements Runnable {
@@ -18,16 +20,12 @@ public class Runner implements Runnable {
     public void run() {
         System.out.println("run " + submit.id);
         System.out.println(submit.source);
-        /*
         try {
             compile();
             submit.status = 100;
-        } catch (OJException ex) {
-            submit.status = ex.getCode();
-            submit.detail = ex.getMessage();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        */
-        test();
         submit.finishTime = new Date();
         submit.status = 100;
         submit.save();
@@ -36,52 +34,31 @@ public class Runner implements Runnable {
         Judger.start();
     }
 
-    public void test() {
-        FileUtils.deleteRecursive("temp", true);
-        (new File("temp/exroot")).mkdirs();
-        /*
-        try {
-            PrintWriter writer = new PrintWriter(new File("temp/exroot/submit.c"));
-            writer.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        */
-    }
-
-    public void compileSubmit() throws OJException {
+    public void compile() throws IOException {
         String error = null;
         switch (submit.language) {
             case 0: {
-                try {
-                    FileUtils.deleteRecursive("temp", true);
-                    (new File("temp/exroot")).mkdirs();
-                    PrintWriter writer = new PrintWriter(new File("temp/exroot/submit.c"));
-                    writer.print(submit.source);
-                    writer.close();
-                    LangC langC = new LangC();
-                    langC.compile();
-                    File compiled = new File("temp/exroot/a.out");
-                    if (compiled.exists()) {
-                        return;
-                    } else {
-                        File compileErrorFile = new File("/temp/exroot/compile_err.txt");
-                        FileInputStream fis = new FileInputStream(compileErrorFile);
-                        byte[] data = new byte[(int)compileErrorFile.length()];
-                        fis.read(data);
-                        fis.close();
-                        error = new String(data, "UTF-8");
-                    }
-                } catch (FileNotFoundException e) {
-                    System.out.print("File not found");
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                FileUtils.deleteRecursive("temp", true);
+                (new File("temp/exroot")).mkdirs();
+                PrintWriter writer = new PrintWriter(new File("temp/exroot/submit.c"));
+                writer.print(submit.source);
+                writer.close();
+                LangC langC = new LangC();
+                langC.compile();
+                File compiled = new File("temp/exroot/a.out");
+                if (compiled.exists()) {
+                    return;
+                } else {
+                    File compileErrorFile = new File("/temp/exroot/compile_err.txt");
+                    FileInputStream fis = new FileInputStream(compileErrorFile);
+                    byte[] data = new byte[(int)compileErrorFile.length()];
+                    fis.read(data);
+                    fis.close();
+                    error = new String(data, "UTF-8");
                 }
                 break;
             }
         }
-        throw new OJException(200, error);
+        // throw new OJException(200, error);
     }
 }
