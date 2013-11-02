19f10b9d6e21310b72b66978ab355358661a7c98
Zhou Xinzi <imdreamrunner@gmail.com>
2013-11-02 16:33:01 +0800

    working version

--- app/judger/Runner.java
+++ app/judger/Runner.java
@@ -20,45 +20,48 @@ public class Runner implements Runnable {
     public void run() {
         System.out.println("run " + submit.id);
         System.out.println(submit.source);
-        try {
-            compile();
-            submit.status = 100;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        submit.finishTime = new Date();
-        submit.status = 100;
-        submit.save();
         System.out.println("ran " + submit.id);
+        judge();
         Judger.running = false;
         Judger.start();
     }
 
-    public void compile() throws IOException {
-        String error = null;
-        switch (submit.language) {
-            case 0: {
-                FileUtils.deleteRecursive("temp", true);
-                (new File("temp/exroot")).mkdirs();
-                PrintWriter writer = new PrintWriter(new File("temp/exroot/submit.c"));
-                writer.print(submit.source);
-                writer.close();
-                LangC langC = new LangC();
-                langC.compile();
-                File compiled = new File("temp/exroot/a.out");
-                if (compiled.exists()) {
-                    return;
-                } else {
-                    File compileErrorFile = new File("/temp/exroot/compile_err.txt");
-                    FileInputStream fis = new FileInputStream(compileErrorFile);
-                    byte[] data = new byte[(int)compileErrorFile.length()];
-                    fis.read(data);
-                    fis.close();
-                    error = new String(data, "UTF-8");
+    public void judge() {
+        compile();
+        submit.status = 100;
+        submit.finishTime = new Date();
+        submit.save();
+    }
+
+    public boolean compile() {
+        try {
+            switch (submit.language) {
+                case 0: {
+                    FileUtils.deleteRecursive("temp", true);
+                    (new File("temp/exroot")).mkdirs();
+                    PrintWriter writer = new PrintWriter(new File("temp/exroot/submit.c"));
+                    writer.print(submit.source);
+                    writer.close();
+                    LangC langC = new LangC();
+                    langC.compile();
+                    File compiled = new File("temp/exroot/a.out");
+                    if (compiled.exists()) {
+                        return true;
+                    } else {
+                        File compileErrorFile = new File("temp/exroot/compile_err.txt");
+                        FileInputStream fis = new FileInputStream(compileErrorFile);
+                        byte[] data = new byte[(int)compileErrorFile.length()];
+                        fis.read(data);
+                        fis.close();
+                        String error = new String(data, "UTF-8");
+                        submit.detail = error;
+                    }
+                    break;
                 }
-                break;
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        // throw new OJException(200, error);
+        return false;
     }
 }
